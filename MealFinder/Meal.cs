0e128db89fd7ff47a885a7c99ce542bd18d72eda
lukatur <Turas97@gmail.com>
2021-03-03 12:40:47 +0100

    First commit

---
+++ MealFinder/Meal.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text.Json.Serialization;
+using System.Threading;
+
+namespace MealFinder
+{
+	public class Meal
+	{
+		[JsonPropertyName("strMeal")]
+		public string Name { get; set; }
+
+		[JsonPropertyName("strCategory")]
+		public string Category { get; set; }
+
+		[JsonPropertyName("strArea")]
+		public string Area { get; set; }
+
+		[JsonPropertyName("strInstructions")]
+		public string Instruction { get; set; }
+
+		[JsonPropertyName("strMealThumb")]
+		public string Photo { get; set; }
+
+		[JsonPropertyName("strYoutube")]
+		public string YoutubeLink { get; set; }
+
+		[JsonPropertyName("strIngredient1")]
+		public string Ingredient1 { get; set; }
+
+		[JsonPropertyName("strIngredient2")]
+		public string Ingredient2 { get; set; }
+
+		[JsonPropertyName("strIngredient3")]
+		public string Ingredient3 { get; set; }
+
+		[JsonPropertyName("strIngredient4")]
+		public string Ingredient4 { get; set; }
+
+		[JsonPropertyName("strIngredient5")]
+		public string Ingredient5 { get; set; }
+
+		[JsonPropertyName("strIngredient6")]
+		public string Ingredient6 { get; set; }
+
+		[JsonPropertyName("strIngredient7")]
+		public string Ingredient7 { get; set; }
+
+		[JsonPropertyName("strIngredient8")]
+		public string Ingredient8 { get; set; }
+
+		[JsonPropertyName("strIngredient9")]
+		public string Ingredient9 { get; set; }
+
+		[JsonPropertyName("strIngredient10")]
+		public string Ingredient10 { get; set; }
+
+		[JsonPropertyName("strIngredient11")]
+		public string Ingredient11 { get; set; }
+
+		[JsonPropertyName("strIngredient12")]
+		public string Ingredient12 { get; set; }
+
+		[JsonPropertyName("strIngredient13")]
+		public string Ingredient13 { get; set; }
+
+		[JsonPropertyName("strIngredient14")]
+		public string Ingredient14 { get; set; }
+
+		[JsonPropertyName("strIngredient15")]
+		public string Ingredient15 { get; set; }
+
+		[JsonPropertyName("strIngredient16")]
+		public string Ingredient16 { get; set; }
+
+		[JsonPropertyName("strIngredient17")]
+		public string Ingredient17 { get; set; }
+
+		[JsonPropertyName("strIngredient18")]
+		public string Ingredient18 { get; set; }
+
+		[JsonPropertyName("strIngredient19")]
+		public string Ingredient19 { get; set; }
+
+		[JsonPropertyName("strIngredient20")]
+		public string Ingredient20 { get; set; }
+
+		[JsonPropertyName("strMeasure1")]
+		public string Measure1 { get; set; }
+
+		[JsonPropertyName("strMeasure2")]
+		public string Measure2 { get; set; }
+
+		[JsonPropertyName("strMeasure3")]
+		public string Measure3 { get; set; }
+
+		[JsonPropertyName("strMeasure4")]
+		public string Measure4 { get; set; }
+
+		[JsonPropertyName("strMeasure5")]
+		public string Measure5 { get; set; }
+
+		[JsonPropertyName("strMeasure6")]
+		public string Measure6 { get; set; }
+
+		[JsonPropertyName("strMeasure7")]
+		public string Measure7 { get; set; }
+
+		[JsonPropertyName("strMeasure8")]
+		public string Measure8 { get; set; }
+
+		[JsonPropertyName("strMeasure9")]
+		public string Measure9 { get; set; }
+
+		[JsonPropertyName("strMeasure10")]
+		public string Measure10 { get; set; }
+
+		[JsonPropertyName("strMeasure11")]
+		public string Measure11 { get; set; }
+
+		[JsonPropertyName("strMeasure12")]
+		public string Measure12 { get; set; }
+
+		[JsonPropertyName("strMeasure13")]
+		public string Measure13 { get; set; }
+
+		[JsonPropertyName("strMeasure14")]
+		public string Measure14 { get; set; }
+
+		[JsonPropertyName("strMeasure15")]
+		public string Measure15 { get; set; }
+
+		[JsonPropertyName("strMeasure16")]
+		public string Measure16 { get; set; }
+
+		[JsonPropertyName("strMeasure17")]
+		public string Measure17 { get; set; }
+
+		[JsonPropertyName("strMeasure18")]
+		public string Measure18 { get; set; }
+
+		[JsonPropertyName("strMeasure19")]
+		public string Measure19 { get; set; }
+
+		[JsonPropertyName("strMeasure20")]
+		public string Measure20 { get; set; }
+
+	}
+}
---
+++ MealFinder/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace MealFinder
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var meal = await ProcessMeal();
+
+			PrintRecipe(meal);
+
+		}
+
+		private static async Task<Meal> ProcessMeal()
+		{
+			HttpClient client = new HttpClient();
+			string filePath = @"..\..\..\files\Meal.txt";
+			client.DefaultRequestHeaders.Accept.Clear();
+			client.DefaultRequestHeaders.Accept.Add(
+				new MediaTypeWithQualityHeaderValue("application/json"));
+			client.DefaultRequestHeaders.Add("x-rapidapi-key", "d0d13c1d59mshf5a6f85d3cec2b8p12f4c4jsnd51"); //key is changed to protect privacy
+			client.DefaultRequestHeaders.Add("x-rapidapi-host", "themealdb.p.rapidapi.com");
+
+			// consume API
+			//string stringTask = await client.GetStringAsync("https://themealdb.p.rapidapi.com/random.php");
+			//stringTask = stringTask.Substring(10, stringTask.Length - 12);
+
+			string stringTask = await File.ReadAllTextAsync(filePath);
+			var meal = JsonSerializer.Deserialize<Meal>(stringTask);
+
+
+			// serialize to file
+			//await File.WriteAllTextAsync(filePath, stringTask);
+
+			return meal;
+
+		}
+
+		private static void PrintRecipe(Meal meal)
+		{
+			Console.WriteLine("Meal name: " + meal.Name);
+			Console.WriteLine("{0} is a {1} {2} dish.", meal.Name, meal.Area.ToLower(), meal.Category.ToLower());
+			Console.WriteLine("\nIndredients:");
+			for (int i = 1; i <= 20; i++)
+			{
+				var ingredient = meal.GetType().GetProperty("Ingredient" + i).GetValue(meal).ToString();
+				var measure = meal.GetType().GetProperty("Measure" + i).GetValue(meal).ToString();
+
+				if (!string.IsNullOrEmpty(ingredient) && !string.IsNullOrEmpty(measure))
+				{
+					Console.WriteLine($"\t{measure} of {ingredient.ToLower()}");
+				}
+			}
+			Console.WriteLine($"\nInstructions:\n{meal.Instruction}");
+		}
+	}
+
+
+}
